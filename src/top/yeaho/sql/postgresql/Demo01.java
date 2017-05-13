0e236602ab13a217ffbbdfd65d6b5e0c9bb77563
upnow <112266@outlook.com>
2017-05-13 22:57:27 +0800

    gaofeng

--- src/top/yeaho/sql/postgresql/Demo01.java
+++ src/top/yeaho/sql/postgresql/Demo01.java
@@ -38,6 +38,7 @@ public class Demo01 {
 	        	System.out.println(rSet.getString(1));
 	        	/*测试github*/
 	        	//欢迎使用
+	        	/*Ubuntu高峰分支的测试*/
 	        	
 	        }
 			stmt.close();
