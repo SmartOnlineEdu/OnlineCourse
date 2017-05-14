eb3ae174be030f84b654dfda3c3a820dfe74d659
upnow <112266@outlook.com>
2017-05-14 14:05:56 +0800

    gaofeng

---
+++ src/top/yeaho/entities/Course.java
@@ -0,0 +1,21 @@
+package top.yeaho.entities;
+
+
+public class Course {
+	private int id;
+	private String courseName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	
+}
