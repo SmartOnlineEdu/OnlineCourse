17e34c2ddaa54dee08b3519f792bbb136582d829
upnow <112266@outlook.com>
2017-05-16 01:31:05 +0800

    gaofeng

---
+++ src/top/yeaho/entities/SchoolClass.java
@@ -0,0 +1,32 @@
+package top.yeaho.entities;
+
+public class SchoolClass {
+	private int schoolClassId;
+	private String schoolclassNo;
+	private String schoolclassName;
+	private String school;
+	public int getSchoolClassId() {
+		return schoolClassId;
+	}
+	public void setSchoolClassId(int schoolClassId) {
+		this.schoolClassId = schoolClassId;
+	}
+	public String getSchoolclassNo() {
+		return schoolclassNo;
+	}
+	public void setSchoolclassNo(String schoolclassNo) {
+		this.schoolclassNo = schoolclassNo;
+	}
+	public String getSchoolclassName() {
+		return schoolclassName;
+	}
+	public void setSchoolclassName(String schoolclassName) {
+		this.schoolclassName = schoolclassName;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+}
